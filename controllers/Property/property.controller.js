8aaf42a29e0aa2d85fa619ff4a972ccd03db4437
preet-patil-4055 <preet.patil@retvensservices.com>
2023-10-11 01:23:21 +0530

    update add property api

--- controllers/Property/property.controller.js
+++ controllers/Property/property.controller.js
@@ -1,7 +1,12 @@
 import Randomstring from 'randomstring';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import propertyModel from '../../models/property.js'
 import getCurrentUTCTimestamp from '../../helpers/timestampHelper.js'
+import uploadImageToS3 from '../../helpers/singleImageUploadHelper.js'
 import s3 from "../../utils/url.js"
+
+//upload property controller
 const postProperty = async (req, res) => {
   try {
     const { userId,
@@ -15,32 +20,50 @@ const postProperty = async (req, res) => {
       websiteUrl,
       propertyChainName,
       propertyType,
-      hotelLogo } = req.body
+    } = req.body
 
     var hotelLogoId = Randomstring.generate(8)
-    let imageUrl;
-
-    // Upload the file to Amazon s3 Spaces if a file has been selected
-    if (req.file) {
-      const params = {
-        Bucket: 'rown-space-bucket',
-        Key: `hotel_images/${req.file.originalname}`,
-        Body: req.file.buffer,
-        ContentType: req.file.mimetype,
-        ACL: 'public-read'
-      };
-      await s3.upload(params).promise();
-      imageUrl = `https://rown-space-bucket.nyc3.digitaloceanspaces.com/hotel_images/${req.file.originalname}`;
-    }
-
+    
+    var bucket = process.env.bucket;
+    // Upload the file to s3 Spaces if a file has been selected
+    // if (req.file) {
+    //   const params = {
+    //     Bucket: bucket,
+    //     Key: `hotel_images/${req.file.originalname}`,
+    //     Body: req.file.buffer,
+    //     ContentType: req.file.mimetype,
+    //     ACL: 'public-read'
+    //   };
+    //   await s3.upload(params).promise();
+    //   imageUrl = `https://rown-space-bucket.nyc3.digitaloceanspaces.com/hotel_images/${req.file.originalname}`;
+    // }
+    let image = '';
+    // Upload the image and get the imageUrl
+    const imageUrl = await uploadImageToS3(req.file);
+    console.log(imageUrl);
     const newProperty = new propertyModel({
       userId,
       country,
-      propertyAddress,
-      propertyName,
-      postCode,
-      state,
-      city,
+      propertyAddress: [{
+        propertyAddress,
+        modifiedDate: getCurrentUTCTimestamp()
+      }],
+      propertyName: [{
+        propertyName: propertyName,
+        modifiedDate: getCurrentUTCTimestamp()
+      }],
+      postCode: [{
+        postCode: postCode,
+        modifiedDate: getCurrentUTCTimestamp()
+      }],
+      state: [{
+        state: state,
+        modifiedDate: getCurrentUTCTimestamp()
+      }],
+      city: [{
+        city,
+        modifiedDate: getCurrentUTCTimestamp()
+      }],
       baseCurrency,
       websiteUrl,
       propertyChainName,
--- helpers/timestampHelper.js
+++ helpers/timestampHelper.js
@@ -1,7 +1,7 @@
 function getCurrentUTCTimestamp() {
-    const now = new Date();
-    const utcTimestamp = Math.floor(now.getTime() / 1000); // Convert to seconds
-    return utcTimestamp;
-  }
+  const now = new Date();
+  const utcTimestamp = now.toISOString(); // Convert to ISO string
+  return utcTimestamp;
+}
 
-  export default getCurrentUTCTimestamp
+export default getCurrentUTCTimestamp;

2962247cc4f97a75dfe6fb5a62fd1d4898f0fd23
mayanks12345 <mayank.sahu@retvenstechnologies.com>
2023-10-14 15:30:08 +0530

    amenity update

---
+++ models/amenity.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const amenitySchema = new mongoose.Schema({
+    
+    propertyId:{
+        type: String,
+        default : ''
+    },
+    
+    amenityName: {
+        type : String,
+        default : ''
+    },
+
+    amenityShortCode: {
+        type : String,
+        default : ''
+    },
+
+    amenityType : {
+        type : String,
+        default : ''
+    },
+
+    amenityIcon : {
+        type : String,
+        default : ''
+    },
+
+    amenityIconId : {
+        type : String,
+        default : ''
+    },
+
+});
+const amenity = model('amenity', amenitySchema);
+export default amenity;

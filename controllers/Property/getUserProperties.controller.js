b64fbb1537529fe37e02021d2957d0d4f4d3a2c8
mayanks12345 <mayank.sahu@retvenstechnologies.com>
2023-10-16 16:04:28 +0530

    get user update

---
+++ controllers/Property/getUserProperties.controller.js
@@ -0,0 +1,23 @@
+import Randomstring from "randomstring";
+import * as dotenv from "dotenv";
+dotenv.config();
+import propertyModel from "../../models/property.js";
+
+const userProperty=async(req , res)=>{
+try{
+    const userId  =  req.params.userId
+
+    const userProperties = await propertyModel.find({userId:userId})
+
+    if(userProperties.length>0){
+
+           return res.status(200).json({userProperties, statuscode : 200})
+    } else {
+          res.status(404).json({ error: "no property found", statuscode: 404 })
+
+    }
+}catch(error){
+    res.status(500).json({ error: error.message, statusCode: 500 })
+}
+};  
+export default userProperty;
--- routers/Property/property.routes.js
+++ routers/Property/property.routes.js
@@ -7,10 +7,14 @@ import {postProperty} from '../../controllers/Property/property.controller.js'
 //import postPropertyChain from "../../controllers/Property/propertyChain.controller.js"
 import editProperty from "../../controllers/Property/editPropert.controller.js"
 
+import userProperty from "../../controllers/Property/getUserProperties.controller.js"
+
 const router = express.Router();
 
 router.post("/api/createProperty", upload.single('hotelLogo'), postProperty);
 //router.post("/api/createPropertyChain", upload.single('hotelLogo'), postPropertyChain);
-router.patch("/api/propertyAdditionalDetails",editProperty)
+router.patch("/api/propertyAdditionalDetails",editProperty);
+
+router.get("/api/fetchProperty/:userId",userProperty);
 
 export default router;

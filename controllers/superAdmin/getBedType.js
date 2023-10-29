647019bb45286878179a794382f9d4c4a1c83217
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-10-29 20:56:48 +0530

    update

--- controllers/superAdmin/getBedType.js
+++ controllers/superAdmin/getBedType.js
@@ -5,7 +5,7 @@ import bedTypeModel from "../../models/superAdmin/bedType.js";
 const getBedType = async (req, res) => {
         try{
             const bedTypes = await bedTypeModel.find({}).select('bedTypeId bedType -_id').lean();
-            return res.status(200).json({bedTypes , statusCode: 200});
+            return res.status(200).json({data:bedTypes , statusCode: 200});
 
         }catch (error) {
             return res.status(500).json({ error: error.message, statusCode: 500 });
--- controllers/superAdmin/getPostingRules.js
+++ controllers/superAdmin/getPostingRules.js
@@ -5,7 +5,7 @@ import postingRulesModel from "../../models/superAdmin/postingRules.js";
 const postingRulesModels = async (req, res) => {
        try{
         const postingRules = await postingRulesModel.find({}).select('postingRuleId postingRule -_id ').lean();
-        return res.status(200).json({postingRules , statusCode: 200});
+        return res.status(200).json({data:postingRules , statusCode: 200});
 
        }catch (error) {
         return res.status(500).json({ error: error.message, statusCode: 500 });

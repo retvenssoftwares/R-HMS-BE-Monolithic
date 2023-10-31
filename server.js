e6d4c508a4bea05076c675c7db3043c5c7392d2d
major99 <amandecembersharma@gmail.com>
2023-11-01 02:12:08 +0530

    solving cors error

--- server.js
+++ server.js
@@ -13,17 +13,8 @@ import amenityRoutes from "./routers/Amenities/amenity.routes.js"
 import superAdminRoutes from "./routers/superAdmin/admin.routes.js"
 import testR from './routers/test.router.js'
 const app = express();
-app.use((req, res, next) => {
-  //allow access from every, elminate CORS
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.removeHeader('x-powered-by');
-  //set the allowed HTTP methods to be requested
-  res.setHeader('Access-Control-Allow-Methods','POST');
-  //headers clients can use in their requests
-  res.setHeader('Access-Control-Allow-Headers','Content-Type');
-  //allow request to continue and be handled by routes
-  next();
-});
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(testR)

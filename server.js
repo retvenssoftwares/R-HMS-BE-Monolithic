85f975a6111bae9ddc8cfcc666edb55379c9fd04
major99 <amandecembersharma@gmail.com>
2023-11-01 02:05:43 +0530

    solving cors error

--- server.js
+++ server.js
@@ -16,8 +16,11 @@ const app = express();
 app.use(cors({
   origin: '*'
 }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 app.use(express.json());

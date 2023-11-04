c20cbb4ccd1c7202bf38e6bff8cb4a06144a2516
SHIVAM47-09 <shiv.sharma4709@gmail.com>
2023-11-04 16:12:01 +0530

    pull

--- routers/Booking/booking.js
+++ routers/Booking/booking.js
@@ -2,7 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 import {createResrvation} from "../../controllers/Bookings/booking.js"
+import guestData from "../../controllers/Bookings/guestDetails.js"
 
 router.post("/api/createBooking", createResrvation);
+router.get("api/getGuestDetails", guestData);
 
 export default router;

7df228fde1fbf7adf7db8de0111972c56203406a
SHIVAM47-09 <shiv.sharma4709@gmail.com>
2023-11-08 00:10:33 +0530

    pull

--- controllers/superAdmin/otaPatch.js
+++ controllers/superAdmin/otaPatch.js
@@ -3,11 +3,12 @@ import otaModel from "../../models/superAdmin/otaModel.js";
 const otaPatch = async (req, res) => {
   const { otaId } = req.query;
   try {
-    const findOta = await otaModel.findOne({ "otaId.otaId": otaId },{$set:{isConfig:"true"}});
+    const findOta = await otaModel.findOneAndUpdate({ "otaId.otaId": otaId },{$set:{isConfig:isConfig}},{new:true});
     console.log(findOta )
     if (!findOta) {
       return res.status(404).json({ message: "User not found", statuscode: 404 });
     } else {
+        await findOta.save();
         return res.status(200).json({ message:"config is updated", statuscode:200});
     }
   } catch (error) {

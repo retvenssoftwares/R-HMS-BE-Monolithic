95f650b4d2450baab52971ceeb6dddff327e0387
SHIVAM47-09 <shiv.sharma4709@gmail.com>
2023-11-11 00:46:27 +0530

    push

--- routers/Rooms/ratePlanRouter.js
+++ routers/Rooms/ratePlanRouter.js
@@ -4,11 +4,15 @@ import createDiscountPlan from '../../controllers/Rooms/createDiscountPlan.js';
 import editDiscountPlan from "../../controllers/Rooms/patchDiscountPlan.js";
 import getCompanyRatePlans from '../../controllers/Rooms/getCompanyRatePlans.js';
 import getRatePlansList from '../../controllers/Rooms/getRatePlansList.js'
+import getBarRatePlanViaRoomTypeId from '../../controllers/Rooms/getBarRatePlanViaRoomTypeId.js'
+import getCompanyRatePlanViaRoomTypeId from "../../controllers/Rooms/getCompanyRatePlanViaRoomTypeId.js"
 const router = express.Router();
 
 router.post("/api/ratePlan", ratePlan);
 router.post("/api/addDiscountPlan", createDiscountPlan);
 router.patch("/api/editDiscountPlan", editDiscountPlan);
 router.get("/api/getCompanyPlans", getCompanyRatePlans);
-router.get("/api/getRatePlansList", getRatePlansList)
+router.get("/api/getRatePlansList", getRatePlansList);
+router.get("/api/getBarRatePlanViaRoomTypeId", getBarRatePlanViaRoomTypeId);
+router.get("/api/getCompanyRatePlanViaRoomTypeId", getCompanyRatePlanViaRoomTypeId);
 export default router;

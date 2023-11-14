cd055513217a53ceadcd4547c7ba3cd96e2dde4b
preet-patil-4055 <preet.patil@retvensservices.com>
2023-11-14 21:37:20 +0530

    pull re

--- controllers/Rooms/companiesList.js
+++ controllers/Rooms/companiesList.js
@@ -8,26 +8,27 @@ const companyNames = async (req, res) => {
     const authCodeValue = req.headers['authcode']
     const result = await findUserByUserIdAndToken(userId, authCodeValue)
     if (result.success) {
-    const findCompanyName = await companyModel.find({propertyId}).select('companyName companyId').lean();
+      const findCompanyName = await companyModel.find({ propertyId }).select('companyName companyId').lean();
 
-    if (findCompanyName.length > 0 ) {
-      const foundData = findCompanyName.map((companyData) => {
-        return{
-          ...companyData._doc,
-          companyName:companyData.companyName[0].companyName || "",
-          companyId:companyData.companyId || ""
-        }
-      })
-      return res.status(200).json({data:foundData,statuscode:200});
+      if (findCompanyName.length > 0) {
+        const foundData = findCompanyName.map((companyData) => {
+          return {
+            ...companyData._doc,
+            companyName: companyData.companyName[0].companyName || "",
+            companyId: companyData.companyId || ""
+          }
+        })
+        return res.status(200).json({ data: foundData, statuscode: 200 });
 
+      } else {
+        return res.status(404).json({ message: "No companies found", status: 404 });
+      }
     } else {
-      res.status(404).json({ message: "No companies found", status: 404 });
+      return res.status(result.statuscode).json({ message: result.message, statuscode: result.statuscode });
     }
-  }else {
-    return res.status(result.statuscode).json({ message: result.message, statuscode: result.statuscode });
-}
   } catch (error) {
     console.log("error", error);
+    return res.status(500).json({ message: "Internal Server Error", statuscode: 500 })
   }
 };
 

0949549ea90aeaea39332d7187d8e5dfac962cf8
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-11-14 12:37:28 +0530

    Delete Request api

--- routers/OTA/addMMT.router.js
+++ routers/OTA/addMMT.router.js
@@ -2,7 +2,14 @@ import express from 'express';
 const router = express.Router();
 
 import addMMTRecord from '../../controllers/OTA/addMMT.js';
+import getOta from "../../controllers/OTA/getActiveOta.js"
+import getSource from "../../controllers/OTA/fetchSource.js"
 
+
+/////////////////////////////////////////////////////////
 router.post("/api/addMMT", addMMTRecord);
+router.get("/api/getOtaDetails",getOta);
+router.get("/api/fetchSource",getSource)
+
 
 export default router;

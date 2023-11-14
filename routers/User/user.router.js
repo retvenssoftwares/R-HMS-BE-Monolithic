8d8764e6efe1a9d2ad298be608ddf41a00f6f1b1
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-11-15 02:45:41 +0530

    update in push

--- routers/User/user.router.js
+++ routers/User/user.router.js
@@ -9,12 +9,14 @@ import verifyUser from "../../controllers/User/verifyUser.js"
 import { sessionOut } from '../../controllers/User/sessionOut.js';
 import { sessionIn } from '../../controllers/User/sessionIn.js';
 import logoutUser from "../../controllers/User/logoutUser.js";
-import logInUsers from "../../controllers/User/patchLoginUser.js"
+import logInUsers from "../../controllers/User/patchLoginUser.js";
+import adminLogin from "../../controllers/User/adminLogin.controller.js"
 
 const router = express.Router();
 
 router.post("/api/addUser", postUser);
 router.post("/api/userLogin", userLogin);
+router.post("/api/adminLogin",adminLogin)
 router.patch("/api/userEdit", upload.single('hotelLogo'), editUserOnboarding);
 router.patch("/api/verifyUser", verifyUser)
 router.patch("/api/sessionOut",sessionOut)

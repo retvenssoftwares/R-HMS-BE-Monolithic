444da46288f1b783dc35d50bf601787a9564bbbd
preet-patil-4055 <preet.patil@retvensservices.com>
2023-11-17 14:25:59 +0530

    update company routes

---
+++ models/Notifications/mmtBookingNotification.js
@@ -0,0 +1,50 @@
+import { Schema, model } from 'mongoose';
+import db1 from '../../db/conn.js'
+const bookingNotificationSchema = new Schema({
+    Booking: {
+        bookingId: {
+            type: String,
+            default: ''
+        },
+        customerName: {
+            type: String,
+            default: ''
+        },
+        noOfRooms: {
+            type: Number,
+            default: 0
+        },
+        noOfNights: {
+            type: Number,
+            default: 0
+        },
+        roomTypeName: {
+            type: String,
+            default: ''
+        },
+        checkInDate: {
+            type: String,
+            default: ''
+        },
+        status: {
+            type: String,
+            default: ''
+        },
+        payAtHotelFlag: {
+            type: String,
+            default: ''
+        },
+        bookingTime: {
+            type: Date,
+            default: ''
+        },
+        bookingVendorName: {
+            type: String,
+            default: ''
+        }
+    }
+
+});
+
+const BookingNotificationMMT = db1.model('bookingMMTNotifications', bookingNotificationSchema);
+export default BookingNotificationMMT

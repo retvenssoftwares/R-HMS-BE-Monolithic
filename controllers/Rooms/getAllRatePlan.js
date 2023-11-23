91d6ccdaaa02cf8a76b6d9afbd0766fef595ee25
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-11-23 20:10:13 +0530

    update in package rate plan

--- controllers/Rooms/getAllRatePlan.js
+++ controllers/Rooms/getAllRatePlan.js
@@ -69,20 +69,47 @@ const allRatePlans = async(req,res)=>{
         const matchingRoomTypes = packageRoomTypeDatas.find((room) => room.roomTypeId === rate.roomTypeId
         );
         const roomTypeName = matchingRoomTypes?.roomTypeName[0]?.roomTypeName || '';
-        console.log(roomTypeName)
+       // console.log(roomTypeName)
         return {
             rateType: rate.rateType || '',
             shortCode: rate.shortCode[0].shortCode || '',
             ratePlanName: rate.ratePlanName[0].ratePlanName || '',
             roomTypeName: roomTypeName,
             inclusion: rate.ratePlanInclusion.length || '',
-            ratePlanTotal:rate.ratePlanTotal[0].ratePlanTotal || '',
+            ratePlanTotal:rate.barRates.packageTotal[0].packageTotal || '',
+            extraAdultRate:rate.barRates.extraAdultRate[0].extraAdultRate || '',
+            extraChildRate:rate.barRates.extraChildRate[0].extraChildRate || '',
         };
     });
 
+    //discountPlan
+    //Map roomTypeId from discountPlan
+//     const discountRatePlanData=await discountPlan.find({propertyId}).lean();
+//     //console.log(discountRatePlanData)
+ 
+//   // Extract roomTypeIds from the nested structure within applicableOn
+// const roomTypeIdsInApplicableOn = discountRatePlanData.flatMap((item) => item.applicableOn[0].applicableOn.map((applicable) => applicable.roomTypeId));
+// //console.log(roomTypeIdsInApplicableOn)
+// // Fetch room type data based on the extracted roomTypeIds
+// const discountRoomTypeDatas = await roomTypeModel.find({ roomTypeId: { $in: roomTypeIdsInApplicableOn } });
+// //console.log(discountRoomTypeDatas)
+// const rateType ="discount"
+//     const discountRatePlanResponse = discountRatePlanData.map((rate) => {
+//         const matchingRoomTypes = discountRoomTypeDatas.find((room) => room.roomTypeId === rate.roomTypeId
+//         );
+//         const roomTypeName = matchingRoomTypes?.roomTypeName[0]?.roomTypeName || '';
+//         console.log(roomTypeName)
+//         return {
+//             rateType: rateType || '',
+//             shortCode: rate.shortCode[0].shortCode || '',
+//             ratePlanName:rate.discountName[0].discountName || '',
+//             applicableOn:rate.applicableOn[0].applicableOn || '',
+//         };
+//     });
+
 
      
-     return res.status(200).json({companyRatePlan:CompanyratePlan,barRatePlan:barRatePlanResponse,packageRatePlan:packageRatePlanResponse,statuscode:200})
+  return res.status(200).json({companyRatePlan:CompanyratePlan,barRatePlan:barRatePlanResponse,packageRatePlan:packageRatePlanResponse,statuscode:200})
     
 }catch(err){
     console.error(err)

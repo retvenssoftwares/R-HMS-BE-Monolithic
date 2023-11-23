54c428f49fdf583c0d53007a81d036ba71e7217c
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-11-23 12:26:39 +0530

    update inventory

--- controllers/Rooms/getAllRatePlan.js
+++ controllers/Rooms/getAllRatePlan.js
@@ -9,36 +9,57 @@ const allRatePlans = async(req,res)=>{
      const {propertyId}=req.query;
 
      const companyRatePlan=await companyRatePlanModel.find({propertyId}).lean();
-     console.log("company",companyRatePlan)
-     const roomTypeId=companyRatePlan[0].roomTypeId
-     console.log("RoomType",roomTypeId)
-     const roomTypeName= await roomTypeModel.find({roomTypeId},'roomTypeName -_id')
-     console.log("RoomTypeName",roomTypeName)
+     const barRatePlanData=await barRatePlan.find({propertyId}).lean();
+ 
+     //CompanyRatePlan
+    //Map roomTypeId from companyRatePlan
+    const roomTypeIds = companyRatePlan.map((item) => item.roomTypeId);
+    const roomTypeData = await roomTypeModel.find({ roomTypeId: { $in: roomTypeIds } });
 
-     if(!companyRatePlan){
-        return res.status(404).json({message: "enter valid propertyId",statuscode:404})
-    }
-    //  const discountPlans = await discountPlan.find({propertyId},'shortCode discountName roomTypeId rateTypeId ratePlanTotal extraAdultRate extraChildRate ratePlanInclusion').lean();
-
-     const mealPlans = await mealPlan.find({propertyId},'shortCode mealPlanName mealPlanId ').lean();
+    const CompanyratePlan = companyRatePlan.map((rate) => {
+        const matchingRoomTypes = roomTypeData.filter((room) => room.roomTypeId === rate.roomTypeId).map((foundRoomType) => ({
+                roomTypeName: foundRoomType.roomTypeName[0]?.roomTypeName || '',
+            }))
+    
      
-     const barratePlans = await barRatePlan.find({propertyId},'shortCode ratePlanName roomTypeId rateType mealPlanId ratePlanTotal extraAdultRate extraChildRate inclusion').lean();
-
-     if(companyRatePlan){
-     const ratePlan = companyRatePlan.map((rate)=>{
         return{
-            ...rate._doc,
+            rateType:rate.rateType || '',
             shortCode: rate.shortCode[0].shortCode || '',
             ratePlanName: rate.ratePlanName[0].ratePlanName || '',
-            roomtypeName:roomTypeName[0].roomTypeName[0].roomTypeName  || '',
+            roomTypeName: matchingRoomTypes.length > 0 ? matchingRoomTypes : [], 
             inclusion:rate.ratePlanInclusion.length || '',
             extraAdultRate: rate.barRates.extraAdultRate[0].extraAdultRate || '',
             extraChildRate: rate.barRates.extraChildRate[0].extraChildRate || '',
             ratePlanTotal: rate.barRates.ratePlanTotal[0].ratePlanTotal || '',
         }
-     })
-     return res.status(200).json({data:ratePlan,statuscode:200})
-    }
+    });
+
+
+
+    //BarRatePlan
+    //Map roomTypeId from barRatePlan
+    const barroomTypeIds = barRatePlanData.map((item) => item.roomType.roomTypeId);
+    const roomTypeDatas = await roomTypeModel.find({ roomTypeId: { $in: barroomTypeIds } });
+
+    const barRatePlanResponse = barRatePlanData.map((rate) => {
+        const matchingRoomTypes = roomTypeDatas.filter((room) => room.roomTypeId === rate.roomTypeId).map((foundRoomType) => ({
+            roomTypeName: foundRoomType.roomTypeName[0]?.roomTypeName || '',
+        }))
+
+        return {
+            rateType: rate.rateType || '',
+            shortCode: rate.shortCode[0].shortCode || '',
+            ratePlanName: rate.ratePlanName[0].ratePlanName || '',
+            roomTypeName: matchingRoomTypes.length > 0 ? matchingRoomTypes : [],
+            inclusion: rate.inclusion.length || '',
+            extraAdultRate: rate.barRates.extraAdultRate[0].extraAdultRate || '',
+            extraChildRate: rate.barRates.extraChildRate[0].extraChildRate || '',
+            ratePlanTotal: rate.barRates.ratePlanTotal[0].ratePlanTotal || '',
+        };
+    });
+     
+     return res.status(200).json({companyRatePlan:CompanyratePlan,barRatePlan:barRatePlanResponse,statuscode:200})
+    
 }catch(err){
     console.error(err)
     return res.status(500).json({message: "internal server error",statuscode:500})

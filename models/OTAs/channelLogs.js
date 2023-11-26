d638e4e30185f7cd3f5b7b4eed9847447e809a74
preet-patil-4055 <preet.patil@retvensservices.com>
2023-11-27 02:10:45 +0530

    update restriction apis

--- models/OTAs/channelLogs.js
+++ models/OTAs/channelLogs.js
@@ -1,9 +1,53 @@
-import mongoose from "mongoose"; 
-import db1 from '../../db/conn.js';
+import mongoose from "mongoose";
+import db2 from '../../db/conn2.js';
 
 const channelLogsSchema = mongoose.Schema({
+    propertyId: {
+        type: String,
+        default: ''
+    },
+    otaId: {
+        type: String,
+        default: ""
+    },
+    channelLogs: [{
+        userId: {
+            type: String,
+            default: ""
+        },
+        roomTypeId: {
+            type: String,
+            default: ""
+        },
+        ratePlanId: {
+            type: String,
+            default: ""
+        },
+        deviceType: {
+            type: String,
+            default: ''
+        },
+        ipAddress: {
+            type: String,
+            default: ''
+        },
+        addedOn: {
+            type: String,
+            default: ''
+        },
+        logHeading: {
+            type: String,
+            default: ""
+        },
+        logDescription: {
+            type: String,
+            default: ''
+        }
+    }]
 
-})
+},
+    { versionKey: false }
+)
 
-const channelLogsModel = new mongoose.model("channelLogs", channelLogsSchema);
+const channelLogsModel = db2.model("channelLogs", channelLogsSchema);
 export default channelLogsModel

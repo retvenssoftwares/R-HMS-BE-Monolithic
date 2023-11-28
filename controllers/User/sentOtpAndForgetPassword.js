5f502e32c6b30ab9c8d6234c2c2257d27360cd13
preet-patil-4055 <preet.patil@retvensservices.com>
2023-11-28 18:54:28 +0530

    update patch rate rule api

--- controllers/User/sentOtpAndForgetPassword.js
+++ controllers/User/sentOtpAndForgetPassword.js
@@ -12,18 +12,18 @@ export const forgetPassword = async (req, res) => {
         const data = await verifiedUser.findOne({ email: email })
         count +=1
         if (!data) {
-            return res.status(404).json({ message: "data not found", statusCode: 404 })
+            return res.status(404).json({ message: "data not found", statuscode: 404 })
         }
         const OTP = Randomstring.generate({ charset: 'numeric', length: 6 });
         await verifiedUser.updateOne({ email: email }, { $set: { otp: OTP, time: await getCurrentUTCTimestamp() } })
         await otpVerification(email, OTP);
 
-        return res.status(200).json({ message: `Otp sent on your email ${email}`, statusCode: 200 })
+        return res.status(200).json({ message: `Otp sent on your email ${email}`, statuscode: 200 })
     } else {
 
         const { Otp, password, confirmPassword } = req.body
         // if (!Otp) {
-        //     return res.status(404).json({ message: "some filelds is missing", statusCode: 404 })
+        //     return res.status(404).json({ message: "some filelds is missing", statuscode: 404 })
         // }
         if (Otp) {
             const tenMinutesAgo = new Date(new Date().getTime() - 5 * 60 * 1000);
@@ -32,22 +32,22 @@ export const forgetPassword = async (req, res) => {
             if (details) {
                 await verifiedUser.updateOne({ otp: Otp }, { $set: { otp: "", time: "" } })
                 count = 0
-                return res.status(200).json({ message: "your otp has expired", statusCode: 200 })
+                return res.status(200).json({ message: "your otp has expired", statuscode: 200 })
             } else {
                 const details = await verifiedUser.findOne({ otp: Otp })
 
                 if (!details) {
-                    return res.status(400).json({ message: "Incorrect otp", statusCode: 400 })
+                    return res.status(400).json({ message: "Incorrect otp", statuscode: 400 })
                 }
 
-                return res.status(200).json({ message: "otp verified successfully", statusCode: 200 });
+                return res.status(200).json({ message: "otp verified successfully", statuscode: 200 });
 
 
             }
 
         } else {
             if (!password || !confirmPassword) {
-                return res.status(400).json({ message: "Enter password first", statusCode: 400 });
+                return res.status(400).json({ message: "Enter password first", statuscode: 400 });
             }
             if (password === confirmPassword) {
                 const encryptedPass = encrypt(password);
@@ -70,11 +70,11 @@ export const forgetPassword = async (req, res) => {
 
                 count = 0
 
-                return res.status(200).json({ message: "password updated successfully", statusCode: 200 })
+                return res.status(200).json({ message: "password updated successfully", statuscode: 200 })
 
 
             } else {
-                return res.status(400).json({ message: "password not matched", statusCode: 400 });
+                return res.status(400).json({ message: "password not matched", statuscode: 400 });
             }
         }
 

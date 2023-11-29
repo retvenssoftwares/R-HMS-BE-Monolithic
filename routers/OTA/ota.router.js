03f285a88de3c3d1c5c7d0137151b070dca75acd
preet-patil-4055 <preet.patil@retvensservices.com>
2023-11-29 06:51:08 +0530

    update rateplan api

--- routers/OTA/ota.router.js
+++ routers/OTA/ota.router.js
@@ -7,7 +7,7 @@ import rateRuleUpdate from '../../controllers/OTA/patchRateRules.js';
 import getChannelLogs from '../../controllers/OTA/MMT/channelLogs.js';
 import getOTABookings from '../../controllers/OTA/getOTABookings.js';
 import overViewData from "../../controllers/OTA/Dashboard/overview.js"
-
+import revenueOTAData from "../../controllers/OTA/Dashboard/revenue.js";
 
 /////////////////////////////////////////////////////////
 router.patch("/api/mapRoomType", mapRoomData);
@@ -16,6 +16,6 @@ router.patch("/api/patchRateRule", rateRuleUpdate);
 router.get("/api/getOTABookings", getOTABookings);
 router.get("/api/getChannelLogs", getChannelLogs);
 router.get("/api/getOverViewOfOTA", overViewData);
-
+router.get("/api/getRevenueData", revenueOTAData);
 
 export default router;
